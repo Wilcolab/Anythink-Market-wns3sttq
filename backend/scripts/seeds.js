afdb632f4cd2a995117acef083c6a50cd3bb125d
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2023-03-09 14:49:35 +0000

    script updated

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,60 +1,51 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
-require("dotenv").config();
 const mongoose = require("mongoose");
-require("../models/User");
-require("../models/Item");
-require("../models/Comment");
+const connection = process.env.MONGODB_URI;
+mongoose.connect(connection);
 
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
 
-const connectToDatabase = () => {
-  const connection = process.env.MONGODB_URI;
-  mongoose.connect(connection);
-  mongoose.set("debug", true);
-};
-
-async function main() {
-  connectToDatabase();
-  const verifiedUser = new User({
-    username: 'verifiedUser',
-    email: `verifiedUser@gmail.com`,
-    isVerified: true
-  });
-  await verifiedUser.save();
-
-  const notVerifiedUser = new User({
-      username: 'notVerifiedUser',
-      email: 'notVerifiedUser@gmail.com',
-      isVerified: false,
+async function seedDatabase() {
+    for (let i = 0; i < 100; i++) {
+      // add user
+      const user = { username: `user${i}`, email: `user${i}@gmail.com` };
+      const options = { upsert: true, new: true };
+      const createdUser = await User.findOneAndUpdate(user, {}, options);
+      
+      // add item to user
+      const item = {
+        slug: `slug${i}`,
+        title: `title ${i}`,
+        description: `description ${i}`,
+        seller: createdUser,
+      };
+      const createdItem = await Item.findOneAndUpdate(item, {}, options);
+      
+      // add comments to item
+      if (!createdItem?.comments?.length) {
+        let commentIds = [];
+        for (let j = 0; j < 100; j++) {
+          const comment = new Comment({
+            body: `body ${j}`,
+            seller: createdUser,
+            item: createdItem,
+          });
+          await comment.save();
+          commentIds.push(comment._id);
+        }
+        createdItem.comments = commentIds;
+        await createdItem.save();
+      }
     }
-  );
-  await notVerifiedUser.save();
+  }
 
-  const itemForVerifiedUser = new Item({
-    slug: `verified_seller_item`,
-    title: `Verified seller item`,
-    description: `description for the item`,
-    seller: verifiedUser,
-  });
-  await itemForVerifiedUser.save();
-
-  const itemForNotVerifiedUser = new Item({
-    slug: `not_verified_seller_item`,
-    title: `Not Verified seller item`,
-    description: `description for the item`,
-    seller: notVerifiedUser,
-  });
-  await itemForNotVerifiedUser.save();
-}
-
-main()
+  seedDatabase()
   .then(() => {
-    console.log("Finished DB seeding");
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.log(`Error while running DB seed: ${err.message}`);
-    process.exit(1);
-  });
-  
+  console.log("Finished DB seeding");
+  process.exit(0);
+})
+.catch((err) => {
+  console.log(`Error while running DB seed: ${err.message}`);
+  process.exit(1);
+});
