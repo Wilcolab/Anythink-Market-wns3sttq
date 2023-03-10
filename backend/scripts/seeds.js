87afd2f90b2a101674a5162d19dbdb5afc3deeaa
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2023-03-10 15:58:32 +0000

    updated the code

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,58 +1,85 @@
+//TODO: seeds script should come here, so we'll be able to put some data in our local env
 require("dotenv").config();
-const mongoose = require("mongoose");
 require("../models/User");
 require("../models/Item");
 require("../models/Comment");
 
-const User = mongoose.model("User");
-const Item = mongoose.model("Item");
+const mongoose = require("mongoose");
+const { faker } = require("@faker-js/faker");
 
-const connectToDatabase = () => {
-  const connection = process.env.MONGODB_URI;
-  mongoose.connect(connection);
-  mongoose.set("debug", true);
-};
+const isProduction = process.env.NODE_ENV === "production";
 
-async function main() {
-  connectToDatabase();
-  const verifiedUser = new User({
-    username: 'verifiedUser',
-    email: `verifiedUser@gmail.com`,
-    isVerified: true
-  });
-  await verifiedUser.save();
-
-  const notVerifiedUser = new User({
-      username: 'notVerifiedUser',
-      email: 'notVerifiedUser@gmail.com',
-      isVerified: false,
-    }
-  );
-  await notVerifiedUser.save();
-
-  const itemForVerifiedUser = new Item({
-    slug: `verified_seller_item`,
-    title: `Verified seller item`,
-    description: `description for the item`,
-    seller: verifiedUser,
-  });
-  await itemForVerifiedUser.save();
-
-  const itemForNotVerifiedUser = new Item({
-    slug: `not_verified_seller_item`,
-    title: `Not Verified seller item`,
-    description: `description for the item`,
-    seller: notVerifiedUser,
-  });
-  await itemForNotVerifiedUser.save();
+if (!process.env.MONGODB_URI) {
+	console.warn("Missing MONGODB_URI in env, please add it to your .env file");
 }
 
-main()
-  .then(() => {
-    console.log("Finished DB seeding");
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.log(`Error while running DB seed: ${err.message}`);
-    process.exit(1);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+if (isProduction) {
+} else {
+	mongoose.set("debug", true);
+}
+
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
+
+const testUser = () => {
+	return {
+		username: faker.database.mongodbObjectId(),
+		email: faker.internet.email(),
+		bio: faker.lorem.paragraph(),
+		image: faker.image.avatar(),
+		role: "user",
+		favorites: [],
+		following: [],
+	};
+};
+
+const testItem = () => {
+	const title = faker.commerce.productName() + " " + faker.datatype.uuid();
+	return {
+		title,
+		slug: faker.helpers.slugify(title),
+		description: faker.commerce.productDescription(),
+		image: faker.image.image(),
+		tagList: title.split(" "),
+		comments: [],
+	};
+};
+
+const testComment = () => {
+	return {
+		body: faker.lorem.paragraph(),
+		seller: User.findOne()._id,
+		item: Item.findOne()._id,
+	};
+};
+
+const hundredUsers = faker.helpers.uniqueArray(testUser, 100);
+const hundredItems = faker.helpers.uniqueArray(testItem, 100);
+const hundredComments = faker.helpers.uniqueArray(testComment, 100);
+
+const seedTestData = async () => {
+	// await User.deleteMany({});
+	// await Item.deleteMany({});
+	// await Comment.deleteMany({});
+	await User.insertMany(hundredUsers);
+	await Item.insertMany(hundredItems);
+	await Comment.insertMany(hundredComments);
+	console.log("Database seeded");
+	mongoose.connection.close();
+};
+
+seedTestData()
+	.then(() => {
+		process.exit();
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit();
+	});
