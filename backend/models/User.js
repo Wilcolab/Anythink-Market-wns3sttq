275f8b7ee1eef4a26a2fd3dc14dd2642e93a553e
WilcoApp <hello@wilco.gg>
2024-11-24 08:25:55 +0000

    Initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,10 +29,6 @@ var UserSchema = new mongoose.Schema(
       enum: ["user", "admin"],
       default: "user"
     },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
@@ -89,8 +85,7 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     bio: this.bio,
     image:
       this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
-    following: user ? user.isFollowing(this._id) : false,
-    isVerified: this.isVerified
+    following: user ? user.isFollowing(this._id) : false
   };
 };
 
